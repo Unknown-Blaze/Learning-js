51ade4005e530ec31cd49c903d337301778dc850
Rudra Prasadh Ganesh <grudraprasadh@gmail.com>
2023-10-29 12:38:59 +0800

    First commit - Made a basic increase / decrease / set / reset counter.

---
+++ main.js
@@ -0,0 +1,38 @@
+document.getElementById("increment").onclick = function(){
+    var val = Number(document.getElementById("num").innerHTML);
+    val += 1;
+    document.getElementById("num").innerHTML = String(val);
+}
+document.getElementById("decrement").onclick = function(){
+    var val = Number(document.getElementById("num").innerHTML);
+    val -= 1;
+    document.getElementById("num").innerHTML = String(val);
+}
+document.getElementById("reset").onclick = function(){
+
+    document.getElementById("num").innerHTML = 0;
+}
+
+document.getElementById("submit").onclick = function(){
+
+    let val = document.getElementById("set").value;
+    if (!isNaN(+val) && Boolean(val)){
+        if (val[0] == 0){
+            var i = 0;
+            while (val[i] == 0){
+                i+=1;
+            }
+            val = val.substring(i);
+        }
+        
+        
+        document.getElementById("num").innerHTML = val;
+    }
+    document.getElementById("set").value = "";
+        
+}
+
+document.getElementById("random").onclick = function(){
+
+    document.getElementById("num").innerHTML = Math.ceil(Math.random()*100);
+}
